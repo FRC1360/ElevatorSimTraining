303c352a79b797926e9e60e9a54da6f2df0908cb
Lucasgithubusername <147777106+Lucasgithubusername@users.noreply.github.com>
2023-11-14 20:45:25 -0500

    test
    test

--- src/main/java/frc/robot/lucas elevator sim training/src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/lucas elevator sim training/src/main/java/frc/robot/Robot.java
@@ -1,6 +1,6 @@
-// LUCAS EDITED Copyright (c) FIRST and other WPILib contributors.
-// Luvas Source Software; you can modify and/or share it under the terms of
-// LINE3 the WPILib BSD license file in the root directory of this project.
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
 
 package frc.robot;
 
