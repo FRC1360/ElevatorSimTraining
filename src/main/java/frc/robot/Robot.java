492e64b0647f1945bfad61b17bcd6890b631799b
Lucasgithubusername <147777106+Lucasgithubusername@users.noreply.github.com>
2023-11-14 20:37:43 -0500

    Update Robot.java
    Lucas edited "luvas" on line 2

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,5 +1,5 @@
 // LUCAS EDITED Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
+// Luvas Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
 package frc.robot;
